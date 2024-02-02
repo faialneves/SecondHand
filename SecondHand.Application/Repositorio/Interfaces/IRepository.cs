adcd257ebbbab585706bf8b6ab36aaced8f888a2
faialneves <146934055+faialneves@users.noreply.github.com>
2024-02-02 20:47:18 +0000

    um comentario

---
+++ SecondHand.Application/Repositorio/Interfaces/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondHand.Application.Repositorio.Interfaces
+{
+    public interface IRepository
+    {
+        IEnumerable<SecondHand> GetAll(Exception<Func<SecondHand, booll>>? filter = null, string? includeProperties = null);
+        IEnumerable<SecondHand> Get(Exception<Func<SecondHand, booll>>? filter = null, string? includeProperties = null);
+
+        void Add(SecondHand entity);
+
+        void Update(SecondHand entity);
+
+        void Remove(SecondHand entity);
+
+        void Save();
+
+
+    }
+
+}
---
+++ SecondHand.Infrastructure/Repository/SecondHandRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondHand.Infrastructure.Repository
+{
+    public class SecondHandRepository : ISecondHandRepository
+
+    {
+    }
+}
